6423c27df8dd2a1d34b4ccb45b07195f98424825
Kendall Helmstetter Gelner <Kendall.Gelner@kigisoftware.com>
2019-10-11 16:18:56 -0500

    1.6.1

--- Xcode11/TestProducts-XCode11/Nami.xcframework/ios-x86_64-simulator/Nami.framework/Headers/Nami.h
+++ Xcode11/TestProducts-XCode11/Nami.xcframework/ios-x86_64-simulator/Nami.framework/Headers/Nami.h
@@ -15,4 +15,3 @@ FOUNDATION_EXPORT const unsigned char NamiVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Nami/PublicHeader.h>
 #import <StoreKit/StoreKit.h>
-#import <Nami/Nami-Swift.h>
