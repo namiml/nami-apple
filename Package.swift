8cf970b2dea1f619154ce50ac9c53b1637c80289
Dan Burcaw <92536821+namidan@users.noreply.github.com>
2023-03-12 11:05:52 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         // Nami is a binary XCFramework
         .binaryTarget(
-            name: "Nami",
+            name: "NamiApple",
             url: "https://packages.namiml.com/NamiSDK/Apple/3.0.7/NamiApple.xcframework-3.0.7.zip",
             checksum: "962560124d014dc4fca7b964b2ffda506bc045840b69e5f6021d60cfcfca53a4"
         ),
