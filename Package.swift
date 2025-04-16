b098ebcaf31c48b5ca9442d57ad0f115f6ca7b3c
runner <runner@Mac-1744772619436.local>
2025-04-16 03:18:56 +0000

    3.2.10-beta.01

--- Package.swift
+++ Package.swift
@@ -4,21 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "Nami",
+    name: "NamiApple",
     products: [
         .library(
-            name: "Nami",
-            targets: ["Nami"]),
+            name: "NamiApple",
+            targets: ["NamiApple"]
+        ),
     ],
     dependencies: [
         // No Nami Dependencies
     ],
     targets: [
         // Nami is a binary XCFramework
-	.binaryTarget(
-            name: "Nami",
-            url: "https://packages.namiml.com/NamiSDK/Apple/2.9.4/Nami.xcframework-2.9.4.zip",
-            checksum: "f21cde3dbc4b706e057f4b15a4c2df4949b896d4100e5af107a78321267038c7"
-        )
+        .binaryTarget(
+            name: "NamiApple",
+            url: "https://packages.namiml.com/NamiSDK/Apple/3.2.10-beta.01/NamiApple.xcframework-3.2.10-beta.01.zip",
+            checksum: "add203ef2d3da5cff2edec2b992f157b07b04c1f505b0f626e17f4555ac29653"
+        ),
     ]
 )
