b35ad3e79f4e65ff8db447585be2273db883ae9a
Anka <runner@Mac-1754342836633.local>
2025-08-04 21:36:57 +0000

    3.3.2.5

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         // Nami is a binary XCFramework
         .binaryTarget(
             name: "NamiApple",
-            url: "https://packages.namiml.com/NamiSDK/Apple/3.3.2.4/NamiApple.xcframework-3.3.2.4.zip",
-            checksum: "27540601b42d5ce6a7ed08c8e45b639b0b526bc932b65942227aedadb476a2de"
+            url: "https://packages.namiml.com/NamiSDK/Apple/3.3.2.5/NamiApple.xcframework-3.3.2.5.zip",
+            checksum: "4c1b16626344f00919718643cc289bb0190ff7d63083d508e52dfc712f664a14"
         ),
     ]
 )
